cab2b6c59b7138b590cb0c71370b03ae135e053a
xuepeng <helloxuepeng@gmail.com>
2018-03-27 01:10:26 +0800

    整合功能

--- app/src/main/java/com/example/xuepeng/learncomponents/MainActivity.java
+++ app/src/main/java/com/example/xuepeng/learncomponents/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.xuepeng.learncomponents;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,17 +11,31 @@ import android.widget.Spinner;
 public class MainActivity extends AppCompatActivity {
 
     private Spinner s;
-    private String[] dataSource= new String[]{"HELLO","HI","GOOD","WELL"};
+    private String[] dataSource= new String[]{"选择你想要的功能","选择日期","选择时间","单项选择","多项选择"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Intent i =new Intent(MainActivity.this,)
         s=findViewById(R.id.spinner);
         s.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,dataSource));
         s.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                System.out.println(" 用户选择的是："+dataSource[i]);
+                if (dataSource[i] == dataSource[1]) {
+                    startActivity(new Intent(MainActivity.this,choseDate.class));
+                }
+                else if (dataSource[i] == dataSource[2]) {
+                    startActivity(new Intent(MainActivity.this,choseTime.class));
+                }
+                else if (dataSource[i] == dataSource[3]) {
+                    startActivity(new Intent(MainActivity.this,singleChose.class));
+                }
+                else if (dataSource[i] == dataSource[4]) {
+                    startActivity(new Intent(MainActivity.this,MulChose.class));
+                }
+                else return;
+
 
             }
 
