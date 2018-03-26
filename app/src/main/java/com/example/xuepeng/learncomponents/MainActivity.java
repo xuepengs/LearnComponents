c3ee902e9c2284914d07bd4fe86fb56be19957de
xuepeng <helloxuepeng@gmail.com>
2018-03-26 22:33:26 +0800

    下拉列表

--- app/src/main/java/com/example/xuepeng/learncomponents/MainActivity.java
+++ app/src/main/java/com/example/xuepeng/learncomponents/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.xuepeng.learncomponents;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Spinner s;
+    private String[] dataSource= new String[]{"HELLO","HI","GOOD","WELL"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        s=findViewById(R.id.spinner);
+        s.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,dataSource));
+        s.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                System.out.println(" 用户选择的是："+dataSource[i]);
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 }
